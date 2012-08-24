d64544565ed715c2a265fd54550755ba2518a5b2
Stephen Olsen <steveo@hack-net.org>
2012-08-24 17:36:03 -0400

    You can't play them yet but your reading list now shows up in the new UI

---
+++ resources/public/js/readpod.js
@@ -0,0 +1,53 @@
+// readpod.js
+// stephen olsen
+
+function getAndPlayAudio(article_id) {
+    //Beginnings, will need to more robustly check if it's playing or
+    //if it's loaded and all that fun stuff.
+    $.ajax({
+        url: 'article/' + article_id,
+        success: function(data) {
+            soundManager.createSound({
+                id: article_id,
+                url: data
+            });
+            soundManager.play(article_id);
+        }
+    });
+}
+
+function rowClick(e) {
+    var id = $(e.currentTarget).parent().attr("id");
+    console.log("clicked: " + id);
+    getAndPlayAudio(id);
+}
+
+$(document).ready(function () {
+    soundManager.setup({
+        url: 'swf/',
+        flashVersion: 8,
+        useHighProformance: true,
+        wmode: 'transparent',
+
+        onready: function() {
+
+            var mySound = soundManager.createSound({
+                id: 'aSound',
+                url: '/path/to/an.mp3'
+                // onload: function() { console.log('sound loaded!', this); }
+                // other options here..
+            });
+
+            mySound.play();
+
+        },
+
+        // optional: ontimeout() callback for handling start-up failure
+
+    });
+
+    // Play callbacks, this is going to need reworking.
+    $(".track_title").on("click", rowClick);
+    $(".track_number").on("click", rowClick);
+
+});
